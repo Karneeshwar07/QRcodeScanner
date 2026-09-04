90e0fe8afe7ab01a837dc1d0bf682fcb3efb357b
Karneeshwar07 <karneeshwar07@gmail.com>
2026-09-04 19:32:01 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLC-EhtmE-PrxllWS7E9H0RBVW2SWv4NU",
+  authDomain: "qr-scanner-07.firebaseapp.com",
+  projectId: "qr-scanner-07",
+  storageBucket: "qr-scanner-07.firebasestorage.app",
+  messagingSenderId: "674113177555",
+  appId: "1:674113177555:web:7cd6e92f74d9a4cb491126",
+  measurementId: "G-3EWK0GXR94"
+};
+
+export const COUPON_CODES = {
+  "WELCOME500": 500,
+  "BONUS1000": 1000,
+  "SCANNER2024": 250,
+  "SUMMERBONUS": 750
+};
+
+export const CONSTANTS = {
+  SCAN_COOLDOWN_MS: 2000,
+  TOAST_DURATION_MS: 3000,
+  MAX_HISTORY_ITEMS: 50,
+  CACHE_VERSION: 'v2'
+};
